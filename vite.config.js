8188fe34b9e55ec1416706a65b5d6ead697bcd25
Simon Woo <33717252+simon-woo@users.noreply.github.com>
2024-01-16 11:55:17 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,12 @@
 import { URL, fileURLToPath } from 'node:url'
 // H5项目开启px2vp
 // import cosmixPx2vp from './tools/px2vp'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-export default defineConfig(({ mode }) => {
-
-  console.log('49:26=>', loadEnv(mode, process.cwd()), process.cwd())
+export default defineConfig(() => {
   
   return {
     plugins: [vue(), vueJsx()],
